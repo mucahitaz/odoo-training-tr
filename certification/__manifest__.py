7a542fc5330084c4a131addcae1f52467dd3bc95
mucahitaz <mucahittahaaz@gmail.com>
2019-09-02 16:50:45 +0300

    Odoo training first day projects

---
+++ certification/__manifest__.py
@@ -0,0 +1,22 @@
+{'name': 'Certification',
+ 'summary': 'Defines certification for different purposes',
+ 'author': 'Mucahit',
+ 'website': "https://github.com/mucahitaz",
+ 'category': 'Certification Management',
+ 'version': '12.0.1.0.0',
+ 'license': 'AGPL-3',
+ 'depends': ['base'],
+ 'data': ['security/ir.model.access.csv',
+ 'views/certification_view.xml',
+ 'views/standards_view.xml',
+ 'views/res_partner_view.xml',
+ 'views/certification_bodies.xml',
+
+ ],
+ 'development_status': 'Beta',
+ 'maintainers':['ceefficent'],
+
+
+
+
+ }
